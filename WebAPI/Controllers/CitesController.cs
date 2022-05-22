1f0de1aba9220409905b0776e0a03c23a4ded968
Dipesh Jadhav <Dipxsss@gmail.com>
2022-05-22 21:04:28 +0530

    Added POST and delete method

--- WebAPI/Controllers/CitesController.cs
+++ WebAPI/Controllers/CitesController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAPI.Data;
+using WebAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,15 +19,51 @@ namespace WebAPI.Controllers
         public CitesController(DataContext dc)
         {
             this.dc = dc;
-        }
+        }   
         // GET: api/<CitesController>
         [HttpGet]
-        public IActionResult GetCities()    
+        public async Task<IActionResult> GetCities()    
         {
-            var cities = dc.Cities.ToList();
+            var cities = await dc.Cities.ToListAsync();
             return Ok(cities);
         }
 
+        [HttpPost("add/{cityname}")]
+        public async Task<IActionResult> AddCity(string cityName)
+        {
+            City city = new City();
+            city.Name = cityName;
+
+            await dc.Cities.AddAsync(city);
+
+           await dc.SaveChangesAsync();
+
+            return Ok(city);
+        }
+
+        [HttpPost("Post")]
+        public async Task<IActionResult> AddCityForm(City city)
+        {
+            await dc.Cities.AddAsync(city);
+
+            await dc.SaveChangesAsync();
+
+            return Ok(city);
+        }
+
+        [HttpDelete("delete/{Id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await dc.Cities.FindAsync(id);
+            
+
+              dc.Cities.Remove(city);
+             await dc.SaveChangesAsync();
+
+            return Ok(id);
+
+            
+        }
         // GET api/<CitesController>/5
         [HttpGet("{id}")]
         public string Get(int id)
